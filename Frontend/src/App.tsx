5c9ac94e132953e7dd757b01d7b6f80fed8b7b95
Juanse <juansesosa45@gmail.com>
2025-05-26 17:55:13 -0300

    main

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,33 +1,13 @@
-import { Route, Routes, Navigate } from "react-router-dom";
-import SignUpPage from "./pages/SignUpPage";
-import LoginPage from "./pages/LoginPage";
-import { useAuthContext } from "./context/authUtils";
-import Protectedroute from "./components/routes/ProtectedRoute";
-import Layout from "./Layout/Layout";
-import BoardPage from "./pages/BoardPage";
+import "./App.css";
+
+import SignupPage from "./pages/SignupPage";
 
 function App() {
-  const { authUser } = useAuthContext();
-
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<Layout />}>
-          <Route element={<Protectedroute />}>
-            <Route
-              path="/board/"
-              element={
-                !authUser ? <Navigate to="/login" /> : <BoardPage />
-              }
-            />
-          </Route>
-        </Route>
-      </Routes>
-    </>
+    <div className="bg-neutral-950">
+      <SignupPage></SignupPage>
+    </div>
   );
 }
 
-export default App
+export default App;
